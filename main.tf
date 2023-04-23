7f6eb2bd2264fe7e1438f277383c130de6d6f511
joshi927 <joshipiyush@gmail.com>
2023-04-24 09:44:29 +1000

    Create VPC

--- main.tf
+++ main.tf
@@ -1,141 +1,134 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpcnew" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-   Name = "main"
+   Name = "vpcnew"
   }
 }
 
-resource "aws_subnet" "public0" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "pub1" {
+  vpc_id     = aws_vpc.vpcnew.id
   cidr_block = "10.0.0.0/24"
 
   tags = {
-    Name = "public0"
+    Name = "pub1"
   }
 }
 
-resource "aws_subnet" "public1" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "pub2" {
+  vpc_id     = aws_vpc.vpcnew.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-   Name = "public1"
+    Name = "pub2"
   }
 }
 
-resource "aws_subnet" "private0" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "pvt1" {
+  vpc_id     = aws_vpc.vpcnew.id
   cidr_block = "10.0.2.0/24"
 
   tags = {
-   Name = "private0"
+    Name = "pvt1"
   }
 }
 
-resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "pvt2" {
+  vpc_id     = aws_vpc.vpcnew.id
   cidr_block = "10.0.3.0/24"
 
   tags = {
-   Name = "private1"
+    Name = "pvt2"
   }
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "iggw" {
+  vpc_id = aws_vpc.vpcnew.id
 
   tags = {
-   Name = "main"
+    Name = "iggw"
   }
 }
 
-resource "aws_eip" "nat0" {
+resource "aws_nat_gateway" "natgw1" {
+  allocation_id = aws_eip.nateip1.id
+  subnet_id     = aws_subnet.pub1.id
+
+  tags = {
+    Name = "natgw1"
+  }
+}
+
+resource "aws_nat_gateway" "natgw2" {
+  allocation_id = aws_eip.nateip2.id
+  subnet_id     = aws_subnet.pub2.id
+
+  tags = {
+    Name = "natgw1"
+  }
+}
+
+
+resource "aws_eip" "nateip1" {
   vpc      = true
-
-  tags = {
-   Name = "nat0"
-  }
 }
 
-resource "aws_eip" "nat1" {
+resource "aws_eip" "nateip2" {
   vpc      = true
-
-  tags = {
-   Name = "nat1"
-  }
 }
 
-resource "aws_nat_gateway" "main0" {
-  allocation_id = aws_eip.nat0.id
-  subnet_id     = aws_subnet.public0.id
-
-  tags = {
-   Name = "main0"
-  }
-}
-
-resource "aws_nat_gateway" "main1" {
-  allocation_id = aws_eip.nat1.id
-  subnet_id     = aws_subnet.public1.id
-
-  tags = {
-   Name = "main1"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "routetblpub" {
+  vpc_id = aws_vpc.vpcnew.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.iggw.id
   }
- tags = {
-   Name = "public"
+  tags = {
+    Name = "routetblpub"
   }
 }
 
-resource "aws_route_table" "private0" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "routetblpvt1" {
+  vpc_id = aws_vpc.vpcnew.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main0.id
+    nat_gateway_id = aws_nat_gateway.natgw1.id
   }
- tags = {
-  Name = "private0"
+  tags = {
+    Name = "routetblpvt1"
   }
 }
 
-resource "aws_route_table" "private1" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "routetblpvt2" {
+  vpc_id = aws_vpc.vpcnew.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main1.id
+    nat_gateway_id = aws_nat_gateway.natgw2.id
   }
- tags = {
-   Name = "private1"
+  tags = {
+    Name = "routetblpvt2"
   }
 }
 
-resource "aws_route_table_association" "public0" {
-  subnet_id      = aws_subnet.public0.id
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "pub_tbl_assoc1" {
+  subnet_id      = aws_subnet.pub1.id
+  route_table_id = aws_route_table.routetblpub.id
 }
 
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "pub_tbl_assoc2" {
+  subnet_id      = aws_subnet.pub2.id
+  route_table_id = aws_route_table.routetblpub.id
 }
 
-resource "aws_route_table_association" "private0" {
-  subnet_id      = aws_subnet.private0.id
-  route_table_id = aws_route_table.private0.id
+resource "aws_route_table_association" "pvt_tbl_assoc1" {
+  subnet_id      = aws_subnet.pvt1.id
+  route_table_id = aws_route_table.routetblpvt1.id
 }
 
-resource "aws_route_table_association" "private1" {
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.private1.id
+resource "aws_route_table_association" "pvt_tbl_assoc2" {
+  subnet_id      = aws_subnet.pvt2.id
+  route_table_id = aws_route_table.routetblpvt2.id
 }
